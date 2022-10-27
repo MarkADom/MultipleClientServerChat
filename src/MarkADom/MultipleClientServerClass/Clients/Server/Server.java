5ff06b1aebeb837b7c57cc284d45c07dd1639352
Marco Domingues <67132358+MarkADom@users.noreply.github.com>
2022-10-27 10:16:53 +0100

    Update Server.java

--- src/MarkADom/MultipleClientServerClass/Clients/Server/Server.java
+++ src/MarkADom/MultipleClientServerClass/Clients/Server/Server.java
@@ -13,7 +13,7 @@ public class Server
         ArrayList<Socket> clients = new ArrayList<>();
         HashMap<Socket, String> clientNameList = new HashMap<Socket, String>();
         try (ServerSocket serversocket = new ServerSocket(8085)) {
-            System.out.println("MarkADom.MultipleClientServerClass.MarkADom.MultipleClientServerClass.Clients.Clients.Server is started...");
+            System.out.println("MarkADom Multiple Client Server is started...");
             while (true) {
                 Socket socket = serversocket.accept();
                 clients.add(socket);
